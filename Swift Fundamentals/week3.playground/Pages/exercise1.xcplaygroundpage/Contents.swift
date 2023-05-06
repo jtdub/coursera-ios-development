d4d252982af1e5ccf5922df6057814cf31a50c98
jtdub <amascuba@gmail.com>
2023-05-06 18:44:17 -0500

    week3 exercises

---
+++ Swift Fundamentals/week3.playground/Pages/exercise1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+var goldBars:Int = 0
+
+func unlockTreasureChest(inventory:Int) -> Int {
+    return (inventory + 100)
+}
+
+goldBars = unlockTreasureChest(inventory: goldBars)
+print(goldBars)
+
+goldBars = unlockTreasureChest(inventory: goldBars)
+print(goldBars)
+
+goldBars = unlockTreasureChest(inventory: goldBars)
+print(goldBars)
